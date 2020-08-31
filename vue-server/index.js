4d9c1042828bbc797e7407f568aa8dc5c46d1855
byHT <hung.ta79@outlook.com>
2020-08-31 23:26:15 +0200

    added new random array and timout

--- vue-server/index.js
+++ vue-server/index.js
@@ -3,8 +3,21 @@ const { Socket } = require('dgram');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+function getRandomValueArray(){
+    return Array.from({length: 10}, () =>{ Math.floor(Math.random() * 100)});  
+}
+
 io.on('connect', (socket)=>{
-    console.log('connected');
+    async function sendData(){
+        await new Promise ((resolve) => setTimeout(resolve, 1000)); //wait for one seconds before running next
+
+        const data =  await getRandomValueArray();
+
+        console.log('Sending' + JSON.stringify('data', null , 2));
+        socket.broadcast.emit('newChartData', data);
+    } 
+
+    sendData();
 });
 
 http.listen(5000, () => {
